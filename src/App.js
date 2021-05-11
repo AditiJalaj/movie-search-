8a91606ab88a76801b5c6797c0a98a1bc9936fae
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-11 09:13:54 +0530

    used async/await fetch

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ const App=()=>{
 
 const feature_api="https://api.themoviedb.org/3/discover/movie?sortby=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
 
-  const movies=fetch(feature_api)
-  .then((res)=>res.json())
-  .then((data)=>console.log(data))
+let movies
+ async function fetchMovies(){
+   const response=await fetch(feature_api)
+   //waits until movies is fetched
+
+    movies=await response.json()
+    console.log('inside the fetchMovies'+movies)
+   //movies is an object
+   return movies.results
+ }
+ 
+  const moviesss= fetchMovies()
+  console.log('outside the fetch function moviesss' + moviesss + 'type of moviesss ' + typeof(moviesss))
 
   return(
     <div>
-   {/* {movies.map((movie)=>{
-      <p>{movie}</p>
-    })}   */}
-    </div>
+   <p></p>
+   </div>
   )
 }
 export default App;
