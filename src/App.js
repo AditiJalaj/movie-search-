f12fa2b7f2d60199943076a14a5e5f6f4e146c24
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-11 11:15:04 +0530

    outside fetch movies_results is undfine

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ const App=()=>{
 
 const feature_api="https://api.themoviedb.org/3/discover/movie?sortby=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
 
+
  async function fetchMovies(){
    const response=await fetch(feature_api)
    //waits until movies is fetched
 
     const movies=await response.json()
-    console.log('inside the fetchMovies',movies)
-   
-   return movies
+    console.log('inside the fetchMovies',movies.results)
+    const movies_results=await movies.results
+    
+    return movies_results
+
  }
  
   const moviesss= fetchMovies()
-  console.log(`outside the fetch function moviesss ${moviesss.results}  and type of moviesss ${typeof(moviesss)}`)
+  console.log(`outside the fetch function moviesss ${moviesss}  and type of moviesss ${typeof(moviesss)}`)
 
 
   return(
