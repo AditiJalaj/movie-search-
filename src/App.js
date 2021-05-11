3be259d75bf7bff153ab75a2bad8de2ca1f4d66b
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-11 10:58:35 +0530

    outside fetch

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ const App=()=>{
 
 const feature_api="https://api.themoviedb.org/3/discover/movie?sortby=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
 
-let movies
  async function fetchMovies(){
    const response=await fetch(feature_api)
    //waits until movies is fetched
 
-    movies=await response.json()
-    console.log('inside the fetchMovies'+movies)
-   //movies is an object
-   return movies.results
+    const movies=await response.json()
+    console.log('inside the fetchMovies',movies)
+   
+   return movies
  }
  
   const moviesss= fetchMovies()
-  console.log('outside the fetch function moviesss' + moviesss + 'type of moviesss ' + typeof(moviesss))
+  console.log(`outside the fetch function moviesss ${moviesss.results}  and type of moviesss ${typeof(moviesss)}`)
+
 
   return(
     <div>
