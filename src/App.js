41336bd60ef79130d84224f3b2cdc9ef1054acda
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-12 20:19:04 +0530

    added search movie

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Movies from './Movies'
 
 const App=()=>{
   const [movies,setMovies]=useState(null)
+  let [search,setSearch]=useState('')
 
 const feature_api="https://api.themoviedb.org/3/discover/movie?sortby=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+const search_api='https://api.themoviedb.org/3/search/movie?api_key=9f27855f3a716c4b2b32bb4cf259ed66&query='
 
 useEffect(()=>{
   fetch(feature_api)
@@ -17,10 +19,26 @@ useEffect(()=>{
  
 //console.log('movies after return',movies)
 
+const handleChange=(e)=>{
+ setTimeout(() => {
+   //console.log('this fires after 1.5seconds')
+   search=e.target.value
+   fetch(search_api+search)
+   .then((res)=>res.json())
+   .then((data)=>{return setMovies(data.results)})
+   .then(setSearch(''))
+
+ }, 1500);
+ 
+
+ //where to setSearch('')???
+}
   return(
     <div>
-   
-   {movies && movies.map((movie)=>{
+    <h1>Movie search </h1>
+    <input type="search" onChange={handleChange} placeholder="search.."></input>
+
+    {movies && movies.map((movie)=>{
      return <Movies key={movie.id} {...movie}/>
    })}
    </div>
--- src/Movies.js
+++ src/Movies.js
@@ -1,13 +1,26 @@
-const image_api= 'https://image.tmdb.org/t/p/w400/'
+import {useState } from 'react'
 
+const image_api= 'https://image.tmdb.org/t/p/w400/'
+const search_api='https://api.themoviedb.org/3/search/movie?api_key=9f27855f3a716c4b2b32bb4cf259ed66&query='
 const Movies = ({title,vote_average,overview,poster_path}) => {
+
+    const [search ,setSearch]=useState('')
+    const handleSearch=(e)=>{
+      e.preventDefault()
+      setSearch(e.target.value)
+      fetch(search_api+e.target.value)
+      .then((res)=>res.json())
+      .then((data)=>console.log(data))
+    }
+
     return ( 
          <div> 
-         <h1>MOVIE SEARCH</h1>
-         <img src={image_api+poster_path} alt="poster"/>
+         <div className='movie-container'>
+         <img className="movie-img" src={image_api+poster_path} alt="poster"/>
          <p>{title}</p>
          <span>{vote_average}</span>
-         <p>{overview}</p>
+         <p className="overview">{overview}</p>
+         </div>
         </div>);
 }
  
