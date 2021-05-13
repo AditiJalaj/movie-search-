af3fda38fd789480116a3368d359d901b2d1d81e
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-13 13:36:00 +0530

    getting featured movie after if search is empty

--- src/App.js
+++ src/App.js
@@ -9,34 +9,45 @@ const App=()=>{
 const feature_api="https://api.themoviedb.org/3/discover/movie?sortby=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
 const search_api='https://api.themoviedb.org/3/search/movie?api_key=9f27855f3a716c4b2b32bb4cf259ed66&query='
 
+
 useEffect(()=>{
+  
   fetch(feature_api)
   .then((res)=>{return res.json()})
   .then((data)=>{
     return setMovies(data.results)
   })
-},[])
+},[search])
  
 //console.log('movies after return',movies)
 
-const handleChange=(e)=>{
+const handleSearch=(e)=>{
  setTimeout(() => {
    //console.log('this fires after 1.5seconds')
    search=e.target.value
-   fetch(search_api+search)
+   search && fetch(search_api+search)
    .then((res)=>res.json())
    .then((data)=>{return setMovies(data.results)})
-   .then(setSearch(''))
+   .then((setSearch('')))
 
+   //if search bar is empty
+   /*if(search===''){
+    fetch(feature_api)
+    .then((res)=>{return res.json()})
+    .then((data)=>{
+      return setMovies(data.results)
+    })
+   }  */
+   
  }, 1500);
- 
+  
 
  //where to setSearch('')???
 }
   return(
     <div>
     <h1>Movie search </h1>
-    <input type="search" onChange={handleChange} placeholder="search.."></input>
+    <input type="search" onChange={handleSearch} placeholder="search.."></input>
 
     {movies && movies.map((movie)=>{
      return <Movies key={movie.id} {...movie}/>
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATkttrNdSFZnZJYHu3fHFudE0NmrJwq7M",
+    authDomain: "movie-search-backend.firebaseapp.com",
+    projectId: "movie-search-backend",
+    storageBucket: "movie-search-backend.appspot.com",
+    messagingSenderId: "42082212850",
+    appId: "1:42082212850:web:60b1bcb7d09620d04fa96b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
